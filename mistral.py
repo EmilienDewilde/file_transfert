426bd791965a0539e78ac73bafaaa0bcc63bcce6
Emilien <emilien.dewilde@epitech.eu>
2025-07-07 14:39:47 +0200

    aa

--- mistral.py
+++ mistral.py
@@ -10,13 +10,12 @@ def mistral():
     data = request.get_json()
     prompt = data.get('prompt')
     response: ChatResponse = chat(model='mistral:7b', messages=[
-    {
-        'role': 'user',
-        'content': prompt,
-    },
+        {
+            'role': 'user',
+            'content': prompt,
+        },
     ])
-    print(response)
-    return response, 200
+    return response.model_dump(), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
